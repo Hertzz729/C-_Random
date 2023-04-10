8bb1f5c6ab3a2cfdff84cc0a847fba68b97a3192
Hertzz729 <101437898+Hertzz729@users.noreply.github.com>
2023-04-09 22:57:36 -0600

    Falta corregir la función que verifica al ganador
    Corregir función que verifica al ganador o  si ocurrió un empate

--- JuegoGato.cpp
+++ JuegoGato.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-
 using namespace std;
 
 //matriz Gato
@@ -9,52 +8,84 @@ char matrizG[3][3]={'0','0','0',
 
 // movimientos de cada jugador y juegos ganados
 struct JUGADOR_MV{
-    int n;
-    int m;
+    int n, cero=0, uno=0, dos=0;
+    int m, ceroM=0, unoM=0, dosM=0;
     int win=0;
     char car;
 };
 
 //función donde el jugador escoje su caracter para jugar
-void AsignarChar(struct JUGADOR_MV player){
+void AsignarChar(struct JUGADOR_MV &player){  //pasar la estructura como referencia para que efectue los cambios
     do{
-        cout<<"Elija un caracter del teclado excepto en '0': ";
+        cout<<"elija un caracter del teclado excepto en '0': ";
         cin>>player.car;
     }while(player.car=='0');
     cout<<player.car<<endl;
 }
 
 //función para validar movimiento
-bool ValidMove(int n, int m){
-    if(matrizG[n][m]=='0'){ //si el espacio no esta ocupado se valida
+bool ValidMove(struct JUGADOR_MV player){
+    if(matrizG[player.n][player.m]=='0'){ //si el espacio no esta ocupado se valida
         return true;
     }
     else{
+        cout<<"Esa posición ya esta ocupada, escoja otra "<<endl;
         return false;
     }
 }
 
-//funcion pedir coordenadas
-void Coord(struct JUGADOR_MV cord){
+//función pedir coordenadas
+void Coord(struct JUGADOR_MV &player){ //se tiene que pasar por referencia la estructura si no no efectuara cambios
     cout<<"Digite la condenada n: "; 
-    cin>>cord.n;
+    cin>>player.n;
     cout<<"Digite la condenada m: "; 
-    cin>>cord.m;
+    cin>>player.m;
+}
+
+//función añadir
+void adicion(struct JUGADOR_MV &player, int *punteroN, int *punteroM){
+    switch(*punteroN){
+        case 0:
+        player.cero+=1; break;
+        case 1:
+        player.uno+=1; break;
+        case 2:
+        player.dos+=1; break;
+    }
+    switch(*punteroM){
+        case 0:
+        player.ceroM+=1; break;
+        case 1:
+        player.unoM+=1; break;
+        case 2:
+        player.dosM+=1; break;
+    }
 }
 
 //función para asignar los caracteres a las coordenadas
 void AsignarMatG(struct JUGADOR_MV player){
+    int *puntN=&player.n, *puntM=&player.m;
     do{
         Coord(player);
     
-    }while(ValidMove(player.n, player.m)!=true);
+    }while(ValidMove(player)!=true);
+    
+    adicion(player, puntN, puntM);//---------->MODIFICAR FUNCION
     matrizG[player.n][player.m]=player.car;
 }
 
-//función varificar ganador o tablas
-/*bool VerifGanar(){
-    if
-}*/
+
+//función verificar ganador o empate
+bool ValidWin(struct JUGADOR_MV playerX){
+    if(playerX.cero==3 || playerX.uno==3 || playerX.dos==3){
+        return true;
+    }
+    else if(playerX.ceroM==3 || playerX.unoM==3 || playerX.dosM==3){
+        return true;
+    }
+    return false;
+}
+
 
 //función imprimir matriz
 void imprimir(){
@@ -66,43 +97,48 @@ void imprimir(){
             }
         }
         cout<<endl;
-        if(i<2){
-            for(int k=0; k<2; k++){
-                cout<<"_ _";
-            }
-        }
-        cout<<endl;
-    }
+    }    
 }
 
 void GAME(struct JUGADOR_MV player1, struct JUGADOR_MV player2){
-    int TurnosT=0, turnInd=1;
-    
-    AsignarChar(player1);
-    AsignarChar(player2);
+    int turnInd=1;
+    cout<<"Jugador 1 "; AsignarChar(player1);
+    cout<<"Jugador 2 "; AsignarChar(player2);
     do{
         if(turnInd%2!=0){ //si el turno individual es impar es turno del player1
+            cout<<"Turno del jugador 1:"<<endl;
             AsignarMatG(player1);
             turnInd+=1;
-            cout<<"+++++++"<<endl;
+            if(turnInd>=3){
+                if(ValidWin(player1)==true){
+                    cout<<"El jugador1 ha ganado..."<<endl<<"fin del juego";
+                    break;
+                }
+                
+            }
         }
         else{
-             cout<<"........."<<endl;
+            cout<<"Turno del jugador 2:"<<endl;
             AsignarMatG(player2);
             turnInd+=1;
+            if(turnInd>=4){
+                if(ValidWin(player2)==true){
+                    cout<<"El jugador2 ha ganado"<<endl<<"fin del juego";
+                    break;
+                }
+                
+            }
            
         }
         imprimir();
         
-    }while(TurnosT<9);
-
+    }while(turnInd<10);
+    
+    cout<<"Es un empate";
 }
 
 int main(){
     JUGADOR_MV jugador1, jugador2;
     GAME(jugador1, jugador2);
-    //cout<<jugador1.car<<endl;
-    //cout<<jugador2.car;
-   // imprimir();
     return 0;
 }
